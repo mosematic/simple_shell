cec338b992a27b09561a6cd76aafb1aba252f42b
Mosematic <moselee10@gmail.com>
2022-12-21 21:59:16 +0300

    Simple_shell

--- Tokenize.c
+++ Tokenize.c
@@ -1,61 +1,94 @@
 #include "shell.h"
-/**
- * toksize - number of tokens from getline
- * @str: string to count number of tokens
- * Return: number of tokens
- */
-int toksize(char *str)
-{
-	int tok_size = 0, i = 0;
 
-	if (!str)
+/**
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+
+char **strtow(char *str, char *d)
+{
+	int i, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+			numwords++;
+
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		write(STDIN_FILENO, '\0', 1);
-		return (-1);
+		while (is_delim(str[i], d))
+			i++;
+		k = 0;
+		while (!is_delim(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
 	}
-	while (str[i] != '\0')
-	{
-		/* check if first index is a tab */
-		if (str[i] == '\t')
-			str[i] = ' ';
-		/* check if next index is a tab */
-		if (str[i + 1] == '\t')
-			str[i + 1] = ' ';
-		/**
-		 * if idx is not a space (it's a char)
-		 * AND the idx after is either space or null,
-		 * (meaning end of a word) count it as a token
-		 */
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-			tok_size++;
-		i++;
-	}
-	return (tok_size);
+	s[j] = NULL;
+	return (s);
 }
-/**
- * tokenize - function to get tokens from string
- * and store in an array
- * @str: string to tokenize
- * Return: array
- */
-char **tokenize(char *str)
-{
-	char *token, **arr;
-	int i = 0, words;
 
-	words = toksize(str);
-	if (!words)
+/**
+ * **strtow2 - splits a string into words
+ * @str: the input string
+ * @d: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+char **strtow2(char *str, char d)
+{
+	int i, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
 		return (NULL);
-	arr = malloc(sizeof(char *) * (words + 1));
-	if (!arr)
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
 		return (NULL);
-	token = strtok(str, " \t");
-	while (token != NULL)
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		arr[i] = token;
-		token = strtok(NULL, " \t");
-		i++;
+		while (str[i] == d && str[i] != d)
+			i++;
+		k = 0;
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
 	}
-	arr[i] = NULL;
-	return (arr);
+	s[j] = NULL;
+	return (s);
 }
