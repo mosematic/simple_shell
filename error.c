dd55a5f395cde1ec8fe9083fa934672d959e63af
Mosematic <moselee10@gmail.com>
2022-12-21 21:42:59 +0300

    Simple_shell

--- error.c
+++ error.c
@@ -1,114 +1,141 @@
 #include "shell.h"
+
 /**
- * _itoa - converts int to string
- * @n: integer given
- * Return: new string
+ * _erratoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ *       -1 on error
  */
-
-char *_itoa(int n)
+int _erratoi(char *s)
 {
+	int i = 0;
+	unsigned long int result = 0;
 
-	long x;
-	int count, adjust;
-	char *string;
-
-	/* creating space for max/min int character string */
-	string = malloc(sizeof(char) * 12);
-	/* if string passed is NULL, return NULL */
-	if (string == NULL)
-		return (NULL);
-
-	/* cast the passed integer n into a long variable */
-	x = n;
-
-	/* initialize count to zero */
-	count = 0;
-
-	/* if x is a negative number */
-	if (x < 0)
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
 	{
-		/* append the negative sign */
-		string[count++] = '-';
-		/* convert the number to a positive number */
-		x = x * -1;
-	}
-
-	/* if x is greater than 9 */
-	if (x > 9)
-	{
-		adjust = 10;
-
-		/* if x is > 10, parse through to get to highest place */
-		while (x / adjust >= 10)
-			adjust = adjust * 10;
-
-		/* append the converted highest's place int into the string */
-		string[count++] = ((x / adjust) + '0');
-
-		/* if x is greater than or equal to 10 */
-		while (adjust >= 10)
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			/* move backward through number */
-			adjust = adjust / 10;
-			/* append converted numbers into the string */
-			string[count++] = (((x / adjust) % 10)  + '0');
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
 		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
+/**
+ * print_error - prints an error message
+ * @info: the parameter & return info struct
+ * @estr: string containing specified error type
+ * Return: 0 if no numbers in string, converted number otherwise
+ *        -1 on error
+ */
+void print_error(info_t *info, char *estr)
+{
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
+}
+
+/**
+ * print_d - function prints a decimal (integer) number (base 10)
+ * @input: the input
+ * @fd: the filedescriptor to write to
+ *
+ * Return: number of characters printed
+ */
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
+
+/**
+ * convert_number - converter function, a clone of itoa
+ * @num: number
+ * @base: base
+ * @flags: argument flags
+ *
+ * Return: string
+ */
+char *convert_number(long int num, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
 
 	}
-	/* else if x is a single digit, convert and append to string */
-	else
-		string[count++] = x + '0';
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
 
-	/* append null */
-	string[count] = 0;
-	return (string);
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
 }
+
 /**
- * error - This function returns custom error messages
- * @str: The argument at index 0
- * @line: Line count from getline function
- * @flag: To determine which error message to output
+ * remove_comments - function replaces first instance of '#' with '\0'
+ * @buf: address of the string to modify
+ *
+ * Return: Always 0;
  */
-
-void error(char *str, int line, int flag)
+void remove_comments(char *buf)
 {
-	char *line_counter = NULL;
-	char *shell = _getenv("_");
+	int i;
 
-	line_counter = _itoa(line);
-	write(STDERR_FILENO, shell, _strlen(shell));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, line_counter, _strlen(line_counter));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, str, _strlen(str));
-
-	if (flag == 0)
-		write(STDERR_FILENO, ": not found\n", 13);
-	else if (flag == 1)
-		perror(" ");
-
-	free(line_counter);
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
 }
-/**
- * get_pid - function to get process ID of the calling process
- * Return: pid;
- */
-unsigned int get_pid(void)
-{
-	unsigned int pid;
 
-	pid = getpid();
-	return (pid);
-}
-/**
- * get_ppid - function to return the process ID of the
- * parent process of the calling process
- * Return: ppid
- */
-unsigned int get_ppid(void)
-{
-	unsigned int ppid;
-
-	ppid = getppid();
-	return (ppid);
-}
